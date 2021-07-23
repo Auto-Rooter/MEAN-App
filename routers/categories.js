f429b1ed3c117e982cc37219bb7e4e9ac8658441
AutoRooter <kryptonbot@gmail.com>
2021-07-23 08:06:52 +0100

    Backend: API: Category Create/Delete

--- routers/categories.js
+++ routers/categories.js
@@ -9,6 +9,34 @@ router.get(`/`, async (req, res) =>{
         res.status(500).json({success: false})
     } 
     res.send(categoryList);
-})
+});
 
+router.post('/', async (req, res)=>{
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    });
+
+    category = await category.save();
+
+    if(!category){
+        return res.status(500).send('The Category cannot be created!...')
+    }
+
+    return res.send(category);
+});
+
+
+router.delete('/:id', (req, res)=>{
+    Category.findByIdAndRemove(req.param.id).then(category => {
+        if(category){
+            return res.status(200).json({success: true, msg: 'Catgeory deleted successfully'})
+        }else{
+            return res.status(404).json({success: false, msg: 'Catgeory not found'})
+        }
+    }).catch(err => {
+        return res.status(500).json({success: false, msg: "Server error"})
+    })
+});
 module.exports =router;
